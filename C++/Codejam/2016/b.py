e299502aec6d4ca850c51a688711e64837f3ae7c
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-04-09 17:10:16 +0900

    Prob B

--- C++/Codejam/2016/b.py
+++ C++/Codejam/2016/b.py
@@ -10,38 +10,50 @@ class Cake:
         # min number of time to maneuver
         self.y = 0
         self.S = list(chars)
-        self.top = 0
-        self.checktop()
+        self.ref = len(self.S)
+        #self.checkref()
         # 1st iteration
         self.it1 = True
 
-    def checktop(self):
-        # Determine the top first = 1st - after +
+    def checkref(self):
+        # Determine the ref first = 1st - after +
+        self.ref = int(len(self.S)/2)
+        return
+    ######
         hit = False
-        sz = len(self.S)
+        start = 0 #round(self.ref/2)
+        sz = len(self.S) #self.ref
         try:
-            for i in range(sz):
-                self.top = i
-                if (self.S[i] == '-') and (self.S[i+1] == '+'):
+            self.ref = 1
+            for i in range(start, self.ref):
+
+                if (self.S[i] == '+'): #and (self.S[i+1] == '+'):
+                    if i+1 < len(self.S):
+                        self.ref = i
                     break
                     hit = True
-        except IndexError:
-            pass
 
-        if self.top < 1:
-            self.top = 1
+        except IndexError:
+            return
+
 
     def flip(self):
         N = len(self.S)
         # Flip all characters
         flipped = False
+
+        s = self.S[:self.ref]
+        sprev = s
+        s.reverse()
+        self.S[:self.ref] = s
         #for i in range(N):
-        for i in range(self.top):
+        remain = list()
+        for i in range(self.ref):
             if self.S[i] == '-':
                 self.S[i] = '+'
                 flipped = True
 
-            else:
+            elif (i < N) and ('-' in sprev[i+1:self.ref]):
                 self.S[i] = '-'
                 flipped = True
 
@@ -49,17 +61,10 @@ class Cake:
             self.it1 = False
 
         self.y += 1
-                
-        s = self.S[:self.top]
-        if len(s) > 1:
-            #for j in range(self.top):
-            #    self.S[j] = s[self.top-j-1]
-            s.reverse()
-            self.S[:self.top] = s
-        
-        print("Top:", self.top, ", y:", self.y, ", S:", self.S, ", s:", s)
+
+        print("Ref:", self.ref, ", y:", self.y, ", S:", self.S, ", s:", s)
         #if self.it1:
-        self.checktop()
+        self.checkref()
         return self.y
 
 
@@ -68,10 +73,9 @@ def solve(S):
         return 0
 
     c = Cake(S)
-    i = 0
-    while i < 10 and (c.S != None):
+    while c.S != None:
         c.flip()
-        if '-' not in c.S[:c.top]:
+        if '-' not in c.S[:c.ref]:
             return c.y
     return 0
 
@@ -79,7 +83,7 @@ def solve(S):
 
 #### main
 try:
-    file = "b-test.in" #"B-small.in"
+    file = "b-test.in" #"B-small.in" 
     inp = open(file)
     outp = open(inp.name + ".out", mode='w')
 
