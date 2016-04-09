29d291f9d19f3ab8b7869c977c5c0db3ee6c64a4
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-04-09 15:00:48 +0900

    Prob B start

--- C++/Codejam/2016/b.py
+++ C++/Codejam/2016/b.py
@@ -1,29 +1,82 @@
-﻿#!/usr/bin/env python3
+#!/usr/bin/env python3
 # Codejam 20160408
 # $Id$
-#
+# Problem B:
 
 import sys
 
-def flip(s):
-    for i in len(s):
-        if s[i] == '+':
-            s[i] = '-'
-        else:
-            s[i] = '+'
-    return s.reverse()
+class Cake:
+    def __init__(self, chars):
+        # min number of time to maneuver
+        self.y = 0
+        self.S = list(chars)
+        self.top = int(len(self.S))
+        # 1st iteration
+        self.it1 = True
+
+    def checktop(self):
+        # Determine the top first = 1st - after +
+        hit = False
+        sz = len(self.S)
+        for i in range(sz):
+            self.top = i
+            if (self.S[i] == '-'):
+                if (i < sz-1) and (self.S[i+1] == '+'):
+                    break
+                    hit = True
+
+        if self.top < 1:
+            self.top = 1
+
+    def flip(self):
+        N = len(self.S)
+        # Flip all characters
+        flipped = False
+        #for i in range(N):
+        for i in range(self.top):
+            if self.S[i] == '-':
+                self.S[i] = '+'
+                flipped = True
+
+            elif self.it1:
+                self.S[i] = '-'
+                flipped = True
+
+        if flipped:
+            self.it1 = False
+
+        self.y += 1
+                
+        s = self.S[:self.top]
+        if len(s) > 1:
+            #for j in range(self.top):
+            #    self.S[j] = s[self.top-j-1]
+            s.reverse()
+            self.S[:self.top] = s
+        
+        print("Top:", self.top, ", y:", self.y, ", S:", self.S, ", s:", s)
+        #if self.it1:
+        self.checktop()
+        return self.y
 
 
-# If cols and rows size are the same, t[y][x] can check cols
-def solve(t, n, m):
-    print("hello")
-    pass
+def solve(S):
+    if '-' not in S:
+        return 0
+
+    c = Cake(S)
+    i = 0
+    while i < 10 and (c.S != None):
+        c.flip()
+        if '-' not in c.S[:c.top]:
+            return c.y
+    return 0
 
 
 
 #### main
 try:
-    file = "b-test.in" #"B-small-practice.in"
+    file = "b-test.in" #"B-small.in"
     inp = open(file)
     outp = open(inp.name + ".out", mode='w')
 
@@ -31,16 +84,10 @@ try:
     print(nCases, " test cases")
 
     for k in range(1, nCases + 1):
-        n, m = [int(x) for x in inp.readline().split(sep=' ')]
-        print("n, m:", n, m)
+        S = inp.readline().strip()
+        print("S:", S)
 
-        t = []  # [[0] * n for j in range(m)]
-        # Read the rows
-        for i in range(n):
-            row = [int(x) for x in inp.readline().split(sep=' ')]
-            t.append(row)
-
-        line = "Case #{}{}{}\n".format(k, ': ', solve(t, n, m))
+        line = "Case #{}{}{}\n".format(k, ': ', solve(S))
         sys.stdout.write(line)
         outp.write(line)
 
