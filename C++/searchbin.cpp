d4bf7a61789f954c3d834f761bfa777774f63cfa
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-04-27 18:29:18 +0900

    Partial sum of subsets. Bin search

---
+++ C++/searchbin.cpp
@@ -0,0 +1,27 @@
+/**
+   Binary search using std::lower_bound / upper_bound
+   
+   Makhtar diouf
+   $Id$
+   c++ -g -Wall -std=c++11 searchbin.cpp -o searchbin
+*/
+#include "malib.h"
+
+int main() {
+    using namespace std;
+    clockit();
+    
+    vi v;
+    for(int i=0; i < 100; i++) {
+     v.push_back(rand() % 5);           
+    }
+    v.push_back(20);
+    printf("Vector of size%d: \n", v.size());
+    printV(v);
+    
+    cout << "Search target v[3]: " << *(std::lower_bound(v.begin(), v.end(), -100)) << endl;
+    cout << "Search target " << v[v.size()-1] << ": " << *(std::upper_bound(v.begin(), v.end(), v[v.size()-1])) << endl;
+    
+    clockit();
+    return 0;
+}
