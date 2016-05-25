b1e8f3e470db79dc991e29c5ed14631c59ad1eb0
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-05-25 13:09:35 +0900

    Lambda funcs, error handling

--- Go/maps.go
+++ Go/maps.go
@@ -1,48 +1,75 @@
 // Go hash tables, Maps
+// Makhtar Diouf
 
 package main
 
 import (
-	"math/rand"
 	"fmt"
+	"math/rand"
+	"sort"
 )
 
 type Vertex struct {
+	// Index for sorting
+	i int
 	Lat, Long float64
 }
 
-type mv map[string]Vertex
+type mvertex map[string]Vertex
 
-func (m mv) Len() int {
-	var l = 0
-	for _, _ = range m {
-		l++
-	}
-	return l
-}
 
 func main() {
-	m := make(map[string]Vertex)
+	m := make(mvertex)
+	
 	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
+		0, 40.68433, -74.39967,
 	}
 
-	m["Dakar"] = Vertex{113.09, -45.900}
+	m["Dakar"] = Vertex{1, 113.09, -45.900}
 	m["test"] = Vertex{}
 	
+	var name string
 	for i := 0; i < 5; i++ {
-		m["Loc" + string(i)] = Vertex{rand.Float64(), rand.Float64()}
-		//j >>= 1
+		name = fmt.Sprintf("Loc %v", i)
+		m[name] = Vertex{i + m.Len(), rand.Float64(), rand.Float64()}		
 	}
-	delete(m, "test")	
-	//sort.Sort(m)
-	
+	delete(m, "test")
+	sort.Sort(m)
+	// m.Sort()
+
 	for i, el := range m {
 		println(i, ":\t", el.Lat, el.Long)
 	}
 	println("Length: ", m.Len())
-	
+
 	v, ok := m["Dakar"]
-	fmt.Println("Dakar:", v, "Present?", ok)
+	fmt.Println("\nDakar:", v, "Present?", ok)
+
+}
+
+// Make this type "printable" using the Stringer interface
+func (m mvertex) String() string {
+ return ""//	return fmt.Sprintf("%v %v", m.Lat, m.Long)
+}
+
+// sort.Interface 
+type mv interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
+}
+
+// A method for the type above
+func (m mvertex) Len() int {
+	return len(m)
+}
+
+// For sort.Interface ...Less method
+//type mvertex interface {
+func (m mvertex) Less(i, j int) bool {
+	return i < j
+}
+
+func (m mvertex) Swap(i, j int) {
 	
 }
---
+++ Go/type-assertions.go
@@ -0,0 +1,21 @@
+// +build no-run OMIT
+// Check the underlying data type with an Interface var
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
+}
