9a66eb62b6073c63fa0a04e065810865055d0021
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-07-21 14:22:16 +0900

    DotNet Core

--- C#/HelloDotnetCore/Program.cs
+++ C#/HelloDotnetCore/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using static System.Console;
 
-namespace ConsoleApplication
+namespace ConsoleApp
 {
-    public class Program
+    public class Tryit
     {
         public static void Main(string[] args)
         {
+                        Console.Beep();
             Console.WriteLine("Hello World!");
+            WriteLine("It's " + System.DateTime.Now);
+           
+            WriteLine("\nCalculating Fibonacci numbers");
+            Utils ut = new Utils();
+             for (int i = 0; i < 15; i++)
+            {
+                WriteLine($"{i+1}: {ut.Fibonacci(i)}");
+            }
         }
     }
 }
