f1c9031e2d574168f7334b43fbf214122aa084f9
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-08-21 21:28:44 +0900

    Subsegments

--- C++/strings/subsegments.cpp
+++ C++/strings/subsegments.cpp
@@ -1,6 +1,6 @@
 /**
  Given a paragraph of text, write a program to find the first shortest
- sub-segment that contains each of the given k words at least once. 
+ sub-segment that contains each of the given k words at least once.
  A segment is said to be shorter than other if it contains less number of words.
 
 [Uncomplete BUGGY Version!!]
@@ -40,23 +40,52 @@ int main() {
   string parag, s;
   vector<string> words;
   std::getline(cin, parag);
-  clog << "\nInput Text:\n" << parag;
+  clog << "\nInput Text:\n" << parag << "sz: " << parag.size() << endl;
 
   cin >> numWords;
 
-  unordered_map<string, int> m(parag.size());
+  unordered_map<string, int> m(words.size());
   while (numWords-- > 0) {
     cin >> s;
     words.push_back(s);
+    fprintf(stderr, "W: %s, ", s.c_str());
   }
 
-  for (int i = 0; i < parag.size(); i++) {
-    if (!isspace(parag[i]))
-      s += tolower(parag[i]);  // !watch out
+  long x = 0, y = 0;
+  long segLen = 200000;
+  bool allWfound = false;
+  vector<int> segs;
+
+  s = "";
+
+  for (size_t i = 0; i < parag.size(); i++) {
+    if (isalpha(parag[i]))
+      s += tolower(parag[i]); // !watch out
     else {
       for (auto w : words) {
+        long pos = s.find(w);
+        fprintf(stderr, "\nChecking %s in %s, ret:%ld\n", w.c_str(), s.c_str(),
+                pos);
+
         if (w == s) {
-          m.insert({s, m[s]++});
+          m.insert({w, m[w]++});
+          allWfound = true;
+
+          for (auto tgt : m) {
+            if (tgt.second == 0) {
+              allWfound = false;
+              break;
+            }
+          }
+
+          x++;
+          y = i - s.size();
+          fprintf(stderr, "%s found @ %ld\n", w.c_str(), y);
+          // new segment completed
+          if (allWfound)
+            segLen = std::min(segLen, y - x);
+          if (segLen < 1)
+            segLen = 0;
           break;
         }
       }
@@ -64,11 +93,17 @@ int main() {
     }
   }
 
-  if (m.size() != words.size()) // !nope!
+  clog << "x, y, minSegLen:" << x << " " << y << " " << segLen << endl;
+
+  if (!allWfound) {
     cout << "NO SUBSEGMENT FOUND";
-  else {
+  } else {
+    for (long i = x /* y -segLen*/; i <= y; i++)
+      cout << parag[i];
+
+    fprintf(stderr, "\nm.size:%d, segLen: %ld\n", m.size(), segLen);
     for (auto el : m)
-      cout << el.first << " ";
+      clog << el.first << " " << el.second << endl;
   }
   cout << endl;
   return 0;
