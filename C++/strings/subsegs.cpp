e9ffa30f75c6958c302c3b9a5c01a2875dbc211a
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-08-22 08:16:44 +0900

    Segments near completion

--- C++/strings/subsegs.cpp
+++ C++/strings/subsegs.cpp
@@ -19,23 +19,6 @@
 
 using namespace std;
 
-string solve(string parag, int numWords) {
-  unordered_map<string, int> m(parag.size());
-  string s;
-  for (int i = 0; i < parag.size(); i++) {
-    if (!isspace(parag[i]))
-      s += parag[i];
-    else {
-      m.insert({s, m[s]++});
-      s = "";
-    }
-  }
-  for (auto el : m)
-    cout << el.first << ": " << el.second << endl;
-
-  return "NO SUBSEGMENT FOUND";
-}
-
 int main() {
   int numWords = 0;
   string parag, s;
@@ -44,74 +27,90 @@ int main() {
   clog << "\nInput Text:\n" << parag << "sz: " << parag.size() << endl;
 
   cin >> numWords;
-
-  unordered_map<string, int> m(words.size());
   while (numWords-- > 0) {
     cin >> s;
     words.push_back(s);
     fprintf(stderr, "W: %s, ", s.c_str());
   }
 
-  long x = 0, y = 0;
   int segLen = 200000;
   bool allWfound = false;
-  unordered_map<long, long> segments;
-  numWords = 0;
+  struct _seg {
+    long x = 0;
+    long y = 0;
+    int numWords = 0;
+  };
+
+  vector<_seg> segments;
+
+  unordered_map<string, int> m(words.size());
   s = "";
+  _seg seg;
 
   for (size_t i = 0; i < parag.size(); i++) {
     if (isalpha(parag[i]))
       s += tolower(parag[i]);
     else {
       for (auto w : words) {
+        if (w != s)
+          continue;
 
-        if (w == s) {
-          if (numWords == 0)
-            x = i - s.size();
-          numWords++;
-          m.insert({w, m[w]++});
-          
-          allWfound = true;
-          for (auto tgt : m) {
-            if (tgt.second == 0) {
-              allWfound = false;
-              break;
-            }
-          }
-          y = i;
-          fprintf(stderr, "%s found @ %ld\n", w.c_str(), y);
+        if (seg.numWords == 0)
+          seg.x = i - s.size(); // beginning of 1st word
+        seg.y = i;              // end of last word
+        fprintf(stderr, "%s found @ %ld\n", w.c_str(), seg.y);
 
-          // new segment completed
-          if ((numWords >= words.size()) && allWfound) {
-            segLen = std::min(segLen, numWords);
-            segments.insert({x, y});
-            // For next segment
-            numWords = 0;
-            m.clear();
+        m.insert({w, m[w]++});
+        seg.numWords++;
+        if (m[w] > 1)
+          seg.numWords--;
+
+        allWfound = true;
+        for (auto tgt : m) {
+          fprintf(stderr, "Tgt: %s %d \n", tgt.first.c_str(), tgt.second);
+          if (tgt.second == 0) {
+            allWfound = false;
+            break;
           }
-          break;
         }
+
+        // Completed segment
+        if ((seg.numWords == words.size()) && allWfound) {
+          segLen = std::min(segLen, seg.numWords);
+          segments.push_back(seg);
+
+          // For next segment
+          seg.numWords = 0;
+          seg.x = 0;
+          seg.y = 0;
+          allWfound = false;
+          m.clear();
+        }
+        break;
       }
       s = "";
     }
   }
 
-  clog << "x, y, minSegLen:" << x << " " << y << " " << segLen << " words" << endl;
-
   if (!allWfound) {
     cout << "NO SUBSEGMENT FOUND\n";
   } else {
-    for (auto s : segments) {
-      fprintf(stderr, "\n\n------Results\nSegment [%ld:%ld] :\n", s.first,
-              s.second);
+    struct {
+      bool operator()(_seg a, _seg b) { return a.numWords < b.numWords; }
+    } shortSeg;
 
-      for (long i = s.first; i <= s.second; i++) {
-        char c = parag[i];
-        if (isalpha(c) || isspace(c))
-          cout << c;
-      }
-      cout << endl;
+    for (auto s : segments) {
+      fprintf(stderr, "\n\n------Results\nSegment [%ld:%ld], numW: %d\n", s.x,
+              s.y, s.numWords);
     }
+    std::sort(segments.begin(), segments.end(), shortSeg);
+
+    for (long i = segments[0].x; i <= segments[0].y; i++) {
+      char c = parag[i];
+      if (isalpha(c) || isspace(c))
+        cout << c;
+    }
+    cout << endl;
   }
   return 0;
 }
