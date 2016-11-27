8e00e23255f732501074cf9985257f0f9b39655c
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-11-27 20:20:38 +0900

    ++

--- C++/hashtable.cpp
+++ C++/hashtable.cpp
@@ -1,12 +1,20 @@
 /**
 Custom implementation of a Generic Hash Table
-
 M < N : mapping large input range into a smaller range for fast retrieval
+
+Could use Horner's method for hashing strings but chose the last few chars of the key...
+as if e.g. mapping phone numbers, the last few digits would generate less collisions;
+the initial digits would likely correlate.
+
+http://www.cs.princeton.edu/courses/archive/spr10/cos226/lectures/10-34HashTables-2x2.pdf
+http://en.algoritmy.net/article/50101/Hash-table
 
 Makhtar
 */
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
+#include <sstream>
 #include <algorithm>
 #include <list>
 #include <utility> // pair
@@ -18,37 +26,35 @@ class HashTab
 {
   private:
     // Number of KeyVals and Hash table size
-    int N, M;
+    uint N, M;
 
     typedef pair<KeyT, ValT> kvpair;
 
-    int hash(KeyT key)
+    uint hash(KeyT key)
     {
-        // Horner's method for hashing strings
-        int hashCode = 0;
+        uint hashCode = 0;
         string s = (string)key;
-        for (int i = 0; i < s.size(); i++)
-            hashCode = s[i] + (31 * hashCode);
+        for (uint i = 1; i < 4; i++)
+            hashCode += s[s.size() - i];
 
-        if (hashCode > M)
-            hashCode %= M;
+        hashCode = (97 * hashCode) % M;
         return hashCode;
     }
 
-    // For colliding keys
-    list<kvpair> siblings;
-
-    // Dynamic array of kv
+    // Dynamic array of kv's lists
     list<kvpair> **table;
 
   public:
-    HashTab(int n = 20)
+    uint nCollisions, tableSize;
+
+    HashTab(uint n = 100)
     {
         this->N = n;
         this->M = n / 5;
+        this->nCollisions = 0;
 
         table = new list<kvpair> *[M];
-        for (int i = 0; i < M; i++)
+        for (uint i = 0; i < M; i++)
             table[i] = new list<kvpair>();
     }
 
@@ -59,12 +65,12 @@ class HashTab
     */
     void put(KeyT key, ValT val)
     {
-        int idx = hash(key);
+        uint idx = hash(key);
         auto spot = table[idx];
         if (spot->size() < 1)
         {
             spot->push_back(std::pair<KeyT, ValT>(key, val));
-            clog << "Hash idx: " << idx << " mapped key-val: " << key << " -> " << val << endl;
+            printf("Hash idx: %02d mapped key-val: %s -> %02d\n", idx, key.c_str(), val);
             return;
         }
 
@@ -74,12 +80,13 @@ class HashTab
         if (it != spot->end())
         {
             it->second = val;
-            clog << "Hash idx: " << idx << "updated key: " << key << endl;
+            printf("Hash idx: %02d updated key: %s\n", idx, key.c_str());
         }
         else
         {
+            this->nCollisions++;
             spot->push_back(kv);
-            clog << "Hash idx: " << idx << " mapped siblings key-val: " << key << " -> " << val << endl;
+            printf("Hash idx: %02d mapped siblings key-val: %s -> %02d\n", idx, key.c_str(), val);
         }
     }
 
@@ -89,8 +96,18 @@ class HashTab
 
 int main()
 {
-    auto m = new HashTab<string, int>();
-    m->put("atest", 10);
-    m->put("b", 62);
+    auto m = new HashTab<string, uint>();
+    stringstream ss;
+    uint items = 0;
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        ss << c;
+        m->put(ss.str(), rand() % 100);
+        items++;
+    }
+    // Test key update
+    m->put("abc", 10);
+
+    printf("Items: %d, Number of collisions: %d %.2f\% \n", items, m->nCollisions, (float)(m->nCollisions * 100 / items));
     return 0;
 }
