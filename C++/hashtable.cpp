082e208d1233f5956f372286f873608e909e05cd
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-11-28 08:16:48 +0900

    With standard hashing utility

--- C++/hashtable.cpp
+++ C++/hashtable.cpp
@@ -2,21 +2,24 @@
 Custom implementation of a Generic Hash Table
 M < N : mapping large input range into a smaller range for fast retrieval
 
-Could use Horner's method for hashing strings but chose the last few chars of the key...
-as if e.g. mapping phone numbers, the last few digits would generate less collisions;
-the initial digits would likely correlate.
+Could use FNV, Mersenne prime or Horner's method for hashing but chose the last
+few chars of the key...
+as if e.g. mapping phone numbers, the last few digits would generate less
+collisions; as the initial digits would likely correlate.
 
+http://en.cppreference.com/w/cpp/utility/hash
 http://www.cs.princeton.edu/courses/archive/spr10/cos226/lectures/10-34HashTables-2x2.pdf
 http://en.algoritmy.net/article/50101/Hash-table
 
 Makhtar
 */
-#include <iostream>
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
-#include <sstream>
-#include <algorithm>
+#include <functional> // std::hash
+#include <iostream>
 #include <list>
+#include <sstream>
 #include <utility> // pair
 
 using namespace std;
@@ -30,6 +33,10 @@ class HashTab
 
     typedef pair<KeyT, ValT> kvpair;
 
+    // std hashing utility, generates ulong values
+    std::hash<KeyT> hashFunc;
+
+    // Custom hash function
     uint hash(KeyT key)
     {
         uint hashCode = 0;
@@ -59,38 +66,52 @@ class HashTab
     }
 
     /**
-    Map a key-val pair
-    If the key exist, updates the value
-    If collided, push_back in a list of siblings
-    */
+  Map a key-val pair
+  If the key exist, updates the value
+  If collided, push_back in a list of siblings
+  */
     void put(KeyT key, ValT val)
     {
-        uint idx = hash(key);
+        uint idx = hashFunc(key) % M; // hash(key);
         auto spot = table[idx];
-        if (spot->size() < 1)
-        {
-            spot->push_back(std::pair<KeyT, ValT>(key, val));
-            printf("Hash idx: %02d mapped key-val: %s -> %02d\n", idx, key.c_str(), val);
-            return;
-        }
 
-        // Check duplicates
-        kvpair kv(key, val);
-        auto it = std::find(spot->begin(), spot->end(), kv);
-        if (it != spot->end())
+        // Check duplicates - O(n) sequential search
+        for (auto it = spot->begin(); it != spot->end(); it++)
         {
-            it->second = val;
-            printf("Hash idx: %02d updated key: %s\n", idx, key.c_str());
+            if (it->first == key)
+            {
+                it->second = val;
+                printf("Hash idx: %02d updated key: %s -> %02d\n", idx, key.c_str(), val);
+                return;
+            }
         }
-        else
+        string msg = "Hash idx: %02d mapped %s -> %02d\n";
+        if (spot->size() > 1)
         {
             this->nCollisions++;
-            spot->push_back(kv);
-            printf("Hash idx: %02d mapped siblings key-val: %s -> %02d\n", idx, key.c_str(), val);
+            msg = "Hash idx: %02d mapped siblings %s -> %02d\n";
         }
+        kvpair kv(key, val);
+        // O(1) insertion and removal
+        spot->push_back(kv);
+        printf(msg.c_str(), idx, key.c_str(), val);
     }
 
-    ValT get(KeyT key);
+    ValT get(KeyT key)
+    {
+        uint idx = hashFunc(key) % M; // hash(key);
+        auto spot = table[idx];
+        for (auto it = spot->begin(); it != spot->end(); it++)
+        {
+            if (it->first == key)
+            {
+                printf("Found key %s -> %d, at index %d\n", key.c_str(), it->second, idx);
+                return it->second;
+            }
+        }
+        return (ValT)-1;
+    }
+
     void listTable();
 };
 
@@ -102,12 +123,14 @@ int main()
     for (char c = 'a'; c <= 'z'; c++)
     {
         ss << c;
+        printf("%d\t", items);
         m->put(ss.str(), rand() % 100);
         items++;
     }
     // Test key update
     m->put("abc", 10);
-
-    printf("Items: %d, Number of collisions: %d %.2f\% \n", items, m->nCollisions, (float)(m->nCollisions * 100 / items));
+    m->get("abcdefg");
+    printf("Items: %d, Collisions: %d %.2f\% \n", items, m->nCollisions,
+           (float)(m->nCollisions * 100 / items));
     return 0;
 }
